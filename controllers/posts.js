6031a294499417e164ca1b2829992ec78fb1392e
Mindaugas <strumke@yahoo.com>
2020-06-11 22:28:17 +0200

    some fix

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,16 @@
 const postsRouter = require("express").Router();
 const Blog = require("../models/post");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { request } = require("../app");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 postsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -10,7 +20,12 @@ postsRouter.get("/", async (request, response) => {
 postsRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,

e9edb79fe7b4ca3fe2d784aa90785ec3cf1431e5
Mindaugas <strumke@yahoo.com>
2020-06-15 21:29:11 +0200

    new ui

---
+++ build/precache-manifest.4ab3f831eb58b801ee69a1708f106fcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115a33b01fbd95cebb9a7380d6775463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5adb33992b54480e5e",
+    "url": "/static/js/2.2e15948b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2e15948b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0978de90596cc9f9500c",
+    "url": "/static/js/main.6370396e.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);

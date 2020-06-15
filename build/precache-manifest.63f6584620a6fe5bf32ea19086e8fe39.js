54c4adc4f0571ce4d1e85d0052838df74747820b
Mindaugas <strumke@yahoo.com>
2020-06-15 19:32:11 +0200

    added UI

---
+++ build/precache-manifest.63f6584620a6fe5bf32ea19086e8fe39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2caf43fc1d4d68424b8899a41efa12c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67741ab9579396866415",
+    "url": "/static/js/2.0325642f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0325642f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d906f7b4a56e4cfcd65",
+    "url": "/static/js/main.c7950847.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
